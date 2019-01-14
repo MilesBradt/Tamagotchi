1c2eac10023639df62bc17955b018f3d5d4623af
Chris Rudnicky and Miles Bradt <chris@>
2019-01-14 15:48:14 -0800

    set factors for tamagotchi parameters

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import { Tamagotchi } from './tamagotchi.js';
 import $ from 'jquery';
 
-
-
+let tamagotchi = new Tamagotchi();
+tamagotchi.gettingColder();
+tamagotchi.setHunger();
+tamagotchi.setBoredom();
+tamagotchi.setEnergyLevel();
+tamagotchi.setCleanliness();
+tamagotchi.conditonOfTamagotchi();
+tamagotchi.healthOfTamagotchi();
 
 $(document).ready(function() {
-  let testing = new Tamagotchi();
-  testing.gettingColder();
 
-  
-  testing.setHunger();
-  setInterval(() => { document.getElementById("output").innerHTML = "Hunger: " + testing.hunger;
-}, 1000);
+  setInterval(() => { document.getElementById("output").innerHTML = "Hunger: " + tamagotchi.hunger + "<br>" + "Happiness: " + tamagotchi.happiness + "<br>" + "Boredom: " + tamagotchi.boredom + "<br>" + "Energy Level: " + tamagotchi.energyLevel + "<br>" + "Cleanliness: " + tamagotchi.cleanliness + "<br>" + "Health: " + tamagotchi.health ;
+  }, 1000);
 
-
-console.log(testing.hunger);
 });
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -10,6 +10,7 @@ export class Tamagotchi {
     this.energyLevel = 10;
     this.cleanliness = 10;
     this.boredom = 0;
+    this.likesYou = 10;
   }
 
   gettingColder() {
@@ -27,7 +28,7 @@ export class Tamagotchi {
   }
 
   warmUp() {
-    this.warmthLevel += 2
+    this.warmthLevel += 2;
   }
 
   death() {
@@ -41,15 +42,15 @@ export class Tamagotchi {
   readyToHatch() {
     setInterval(() => {
       this.hatch = true;
-    }, 300000)
+    }, 300000);
   }
 
 
   ////------AFTER HATCH --------/////
   setHunger() {
-  let stopTest = setInterval(() => {
-      if (this.hunger == 0) {
-        clearInterval(stopTest);
+    let hungerCounter = setInterval(() => {
+      if (this.hunger == -100) {
+        clearInterval(hungerCounter);
       } else {
         this.hunger--;
       }
@@ -64,17 +65,15 @@ export class Tamagotchi {
     }
   }
 
-  setBoredom() {
-    setInterval(() => {
-      this.boredom ++;
-    }, 5000)
-  }
-
-
   feed() {
     this.hunger = 10;
   }
 
+  setBoredom() {
+    setInterval(() => {
+      this.boredom ++;
+    }, 5000);
+  }
 
   isItBored() {
     if(this.boredom > 6) {
@@ -85,31 +84,55 @@ export class Tamagotchi {
   }
 
   play() {
-    this.boredom -= 0.5
+    this.boredom -= 0.5;
   }
 
   setEnergyLevel() {
     setInterval(() => {
       this.energyLevel --;
-    }, 10000)
+    }, 10000);
   }
 
   isItTired() {
     if(this.energyLevel >2) {
       return false;
     } else {
-      return true
+      return true;
     }
   }
 
   setCleanliness() {
     setInterval(() => {
       this.cleanliness --;
-    }, 7000)
+    }, 7000);
   }
 
   clean() {
-    this.cleanliness += 0.5
+    this.cleanliness += 0.5;
+  }
+
+  conditonOfTamagotchi() {
+    setInterval(() => {
+      if((-30 <= this.hunger && this.hunger <= 0) || (5 < this.boredom && this.boredom > 10) || (-10 <= this.energyLevel && this.energyLevel <= 0) || (-10 <= this.cleanliness && this.cleanliness <= 0) || (-10 <= this.health && this.health <= 0)) {
+        this.happiness--;
+      }
+    }, 5000);
+  }
+
+  healthOfTamagotchi() {
+    setInterval(() => {
+      if((-50 <= this.hunger && this.hunger <= -10) || (-10 <= this.energyLevel && this.energyLevel <= 0) || (-10 <= this.cleanliness && this.cleanliness <= 0)) {
+        this.health -= 2;
+      }
+    }, 10000);
+  }
+
+  affectionMeter() {
+    setInterval(() => {
+      if(this.happiness <= 5) {
+        this.likesYou -= - 1;
+      }
+    });
   }
 
 }
