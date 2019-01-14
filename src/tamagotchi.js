27b1cdd7f6b8589eb7b6fc6b189933cdf4aaad2e
Chris Rudnicky and Miles Bradt <miles@>
2019-01-14 13:00:09 -0800

    set factors for tamagotchi

---
+++ src/tamagotchi.js
@@ -0,0 +1,108 @@
+export class Tamagotchi {
+
+  constructor(name) {
+    this.name = name;
+    this.warmthLevel = 10;
+    this.hatch = false;
+    this.health = 10;
+    this.hunger = 10;
+    this.happiness = 10;
+    this.energyLevel = 10;
+    this.cleanliness = 10;
+    this.boredom = 0;
+  }
+
+gettingColder() {
+  setInterval(() => {
+    this.warmthLevel -= 2;
+  }, 10000);
+}
+
+isItCold() {
+  if(this.warmthLevel > 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+warmUp() {
+  this.warmthLevel += 2
+}
+
+death() {
+  if(this.warmthLevel <= -8) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+readyToHatch() {
+  setInterval(() => {
+    this.hatch = true;
+  }, 300000)
+}
+
+
+////------AFTER HATCH --------/////
+  setHunger() {
+    setInterval(() => {
+      this.hunger--;
+    }, 1000);
+  }
+
+  isItHungry() {
+    if (this.hunger > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  feed() {
+    this.hunger = 10;
+  }
+
+setBoredom() {
+  setInterval(() => {
+    this.boredom ++;
+  }, 5000)
+}
+
+isItBored() {
+  if(this.boredom > 6) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+play() {
+  this.boredom -= 0.5
+}
+
+setEnergyLevel() {
+  setInterval(() => {
+    this.energyLevel --;
+  }, 10000)
+}
+
+isItTired() {
+  if(this.energyLevel >2) {
+    return false;
+  } else {
+    return true
+  }
+}
+
+setCleanliness() {
+  setInterval(() => {
+    this.cleanliness --;
+  }, 7000)
+}
+
+clean() {
+  this.cleanliness += 0.5
+}
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({sourceMap: true}),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: "Triangle Tracker",
+      title: "Tamagotchi",
       template: './src/index.html',
       inject: 'body'
     })
